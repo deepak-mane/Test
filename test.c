0b2e562bd7817525967e0178fced2a36a4a1017e
Deepak Mane <deepak.mane123@gmail.com>
2019-12-16 22:55:25 -0800

    New file created

---
+++ test.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  testfile
+ *
+ *        Version:  1.0
+ *        Created:  12/16/2019 10:40:49 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  DEEPAK MANE (DM), deepak.mane123@gmail.com
+ *   Organization:  deepaksmane.com
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main(argc, char **argv) {
+  
+
+  return 0;
+ }
